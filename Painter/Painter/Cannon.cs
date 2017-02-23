d2c9b90e6e55ef639a2e6cb4ab50ce954394e053
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-02-23 12:55:42 +0100

    Step 6

--- Painter/Painter/Cannon.cs
+++ Painter/Painter/Cannon.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Painter
 {
-    class Cannon
+    class Cannon : ThreeColorGameObject
     {
+        protected Texture2D cannonBarrel;
+        protected float angle;
+
+        public Cannon(ContentManager content)
+            : base(content.Load<Texture2D>("spr_cannon_red"),
+                   content.Load<Texture2D>("spr_cannon_green"),
+                   content.Load<Texture2D>("spr_cannon_blue"))
+        {
+            cannonBarrel = content.Load<Texture2D>("spr_cannon_barrel");
+            position = new Vector2(72, 405);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(cannonBarrel, position, null, Color.White, angle, new Vector2(34, 34), 1.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(currentColor, position - new Vector2(currentColor.Width, currentColor.Height) / 2, Color.White);
+        }
+
     }
 }
--- Painter/Painter/GameWorld.cs
+++ Painter/Painter/GameWorld.cs
@@ -10,19 +10,27 @@ namespace Painter
 {
     class GameWorld
     {
-        Texture2D background;
+        public Texture2D background;
+        public Cannon cannon;
 
         public GameWorld(ContentManager content)
         {
             background = content.Load<Texture2D>("spr_background");
+            cannon = new Cannon(content);
+        }
+
+        public Cannon Cannon
+        {
+            get { return cannon; }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            cannon.Draw(gameTime, spriteBatch);
             spriteBatch.End();
         }
-
     }
+
 }
